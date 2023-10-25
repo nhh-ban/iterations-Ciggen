afe2d778f472cc21a34fe4ae7f7958c7af4be89e
Sondre Siger <sondre.siger@student.nhh.no>
2023-10-25 16:19:13 +0200

    comit funcitonfiles

---
+++ data_tests.r
@@ -0,0 +1,91 @@
+# This file contains tests to be applied to 
+# the Vegvesen stations-data *after* being transformed
+# to a data frame. 
+# 
+# All tests are packed in a function test_stations_metadata that apples
+# all the aforementioned tests
+
+
+# checking column names in new dataframe is correct. 
+test_stations_metadata_colnames <-
+  function(df) {
+    
+    expected_colnames <- c("id", "name", "latestData", "lat", "lon")
+    
+    if (all(colnames(df) == expected_colnames) == TRUE) {
+      print("PASS: Data has the correct columns")
+    } else{
+      print("FAIL: Columns do not match the correct specification")
+    }
+  }
+
+
+# checking number of samples is reasonable large between 5K-10K. 
+test_stations_metadata_nrows <-
+  function(df) {
+    
+    min_expected_rows <- 5000
+    max_expected_rows <- 10000
+    
+    if (nrow(df) > min_expected_rows & nrow(df) < max_expected_rows) {
+      print("PASS: Data has a reasonable number of rows")
+    } else if (nrow(df) <= min_expected_rows) {
+      print("FAIL: Data has suspiciously few rows")
+    } else {
+      print("FAIL: Data has suspiciously many rows")
+    }
+  }
+
+# checking that the two first columns contains character based values, and the 
+# three last is number based. 
+test_stations_metadata_coltypes <-
+  function(df) {
+    expected_coltypes <-
+      c("character", "character", "double", "double", "double")
+    
+    if (all(df %>%
+            map_chr( ~ typeof(.)) == expected_coltypes) == TRUE) {
+      print("PASS: All cols have the correct specifications")
+    } else{
+      print("FAIL: Columns do not have the correct specification")
+    }
+  }
+  
+# checking if missing stations is reasonable low. 
+test_stations_metadata_nmissing <-
+  function(df) {
+    max_miss_vals <- 200
+    
+    if (df %>% map_int( ~ sum(is.na((.)))) %>% sum(.) < max_miss_vals) {
+      print("PASS: Amount of missing values is reasonable")
+    } else {
+      print("FAIL: Too many missing values in data set")
+    }
+  }
+
+# checking if "latestData" is set to UTC-formatting. 
+test_stations_metadata_latestdata_timezone <-
+  function(df) {
+    
+    if (attr(df$latestData,"tzone")=="UTC") {
+      print("PASS: latestData has UTC-time zone")
+    } else {
+      print("FAIL: latestData does not have expected UTC-time zone")
+    }
+  }
+
+
+# summarize tests, PASS or FAIL. 
+test_stations_metadata <- 
+  function(df){
+    test_stations_metadata_colnames(df)
+    test_stations_metadata_coltypes(df)
+    test_stations_metadata_nmissing(df)
+    test_stations_metadata_nrows(df)
+    test_stations_metadata_latestdata_timezone(df)
+  }
+
+
+
+
+
